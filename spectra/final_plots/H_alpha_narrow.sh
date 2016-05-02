39b001a8aab469318cf53d3778f702115b1e359d
ryanhofmann <ryanhofmann@email.arizona.edu>
2016-05-01 20:16:31 -0700

    Almost there!

--- spectra/final_plots/H_alpha_narrow.sh
+++ spectra/final_plots/H_alpha_narrow.sh
@@ -2,7 +2,7 @@
 
 python <<plot
 from plot import specplot
-specplot(xmin=-1000, xmax=1000, vel=1, narrow=1, scale='log', shift=3, stretch=0, x_label=-900, y_label = 0.2, d_label=1.6)
+specplot(xmin=-1000, xmax=1000, vel=1, narrow=1, scale='lin', shift=3, stretch=0, x_label=-900, y_label = 0.2, d_label=1.6)
 exit()
 plot
 
