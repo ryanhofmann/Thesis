91f681d19289074281f8f4a22b68f0afefc3bf14
ryanhofmann <ryanhofmann@email.arizona.edu>
2016-05-26 17:28:26 -0700

    MNRAS and light curve

--- images/final_plots/plot.py
+++ images/final_plots/plot.py
@@ -12,6 +12,7 @@ def plot(infile, multi=1):
   # Define arrays for plotting
   x = data[:, 0]
   ycols = [1, 3, 5, 7, 9, 11, 13]
+  filters = ['B', 'V', 'R', 'I', 'J', 'H', 'K']
   y = data[:, ycols]
   single = 2 # Column of red magnitudes
   yerrcols = [2, 4, 6, 8, 10, 12, 14]
@@ -31,7 +32,9 @@ def plot(infile, multi=1):
     for i in range(0, len(ycols)):
       ind = np.where(y[:, i] != 0)
       xi, yi, xierr, yierr = x[ind], y[:, i][ind], xerr[ind], yerr[:, i][ind]
-      ax1.errorbar(xi, yi, yierr, xierr, marker='o', linestyle='-')
+      phot = ax1.errorbar(xi, yi, yierr, xierr, marker='o', linestyle='-')
+      color = phot[0].get_color()
+      plt.text(90, yi[1], filters[i], size=10, color=color)
   else:
     ind = np.where(y[:, single] != 0)
     xi, yi, xierr, yierr = x[ind], y[:, single][ind], xerr[ind], yerr[:, single][ind]
@@ -40,11 +43,13 @@ def plot(infile, multi=1):
   # Plot spectra dates
   spec_dates = np.array([67, 91, 106, 116, 149, 191, 235, 400, 458])
   spec_height = np.zeros(len(spec_dates)) + ymin + 0.5*pad
-  plt.plot(spec_dates, spec_height, marker='s', linestyle='None', color='brown')
+  plt.plot(spec_dates, spec_height, marker='|', mew=2, linestyle='None', color='brown')
+  plt.text(260, spec_height[0]+0.1, "spectral epochs", fontsize=10, color="brown")
 
   # Format axes and display
   ax1.set_xlabel('Days since discovery')
   ax1.set_ylabel('Apparent magnitude')
+  ax1.set_xlim(-10, 500)
   ax1.set_ylim(ymax, ymin)
   ax2 = ax1.twinx()
   ax2.set_ylim(ymax - D, ymin - D)
