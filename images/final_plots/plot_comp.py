756f68ec535851c28c9e9f1f013844cdee7fb398
ryanhofmann <ryanhofmann@email.arizona.edu>
2016-10-08 16:17:29 -0600

    More comparisons, corrected spectra

--- images/final_plots/plot_comp.py
+++ images/final_plots/plot_comp.py
@@ -10,7 +10,10 @@ a = np.genfromtxt("phot_data_comp.txt", skip_header=1)
 ind_J13522 = np.where(a[:,1] != 0)[0]
 ind_99em = np.where(a[:,3] != 0)[0]
 ind_98S = np.where(a[:,5] != 0)[0]
-ind_87A = np.where(a[:,7] != 0)[0]
+ind_88Z = np.where(a[:,7] != 0)[0]
+ind_87A = np.where(a[:,8] != 0)[0]
+ind_10jl = np.where(a[:,9] != 0)[0]
+ind_97cy = np.where(a[:,11] != 0)[0]
 T_J = a[ind_J13522,0]
 M_J = a[ind_J13522,1]
 E_J = a[ind_J13522,2]
@@ -20,8 +23,15 @@ E_em = a[ind_99em,4]
 T_S = a[ind_98S,0]
 M_S = a[ind_98S,5]
 E_S = a[ind_98S,6]
+T_Z = a[ind_88Z,0]
+M_Z = a[ind_88Z,7]
 T_A = a[ind_87A,0]
-M_A = a[ind_87A,7]
+M_A = a[ind_87A,8]
+T_jl = a[ind_10jl,0]
+M_jl = a[ind_10jl,9]
+E_jl = a[ind_10jl,10]
+T_cy = a[ind_97cy,0]
+M_cy = a[ind_97cy,11]
 
 # Define calibrations
 # J13522: d = 32.8, A(R) = 1.6
@@ -35,26 +45,41 @@ M_em += peak_em
 # 98S: d = 31.15
 d_S = 31.15
 M_S -= d_S
+# 88Z: d = 36.4
+d_Z = 36.4
+M_Z -= d_Z
 # 87A: d = 18.55
 d_A = 18.55
 M_A -= d_A
+# 10jl: d = 33.41
+d_jl = 33.41
+M_jl -= d_jl
+# 97cy: d = 37.4
+d_cy = 37.4
+M_cy -= d_cy
 
 # Plot data with error bars and two y-axes
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
-ax2.errorbar(T_J, M_J, E_J, 0, fmt='--o', color='r')
-ax2.errorbar(T_em, M_em, yerr=E_em, fmt='-', color='g')
-ax2.errorbar(T_S, M_S, yerr=E_S, fmt='-', color='c')
+ax2.errorbar(T_J, M_J, yerr=E_J, fmt='-', color='r')
+ax2.errorbar(T_cy, M_cy, fmt='-', color='orange')
+ax2.errorbar(T_em, M_em, yerr=E_em, fmt='-', color='y')
+ax2.errorbar(T_S, M_S, yerr=E_S, fmt='-', color='g')
+ax2.errorbar(T_Z, M_Z, fmt='-', color='c')
 ax2.errorbar(T_A, M_A, fmt='-', color='b')
+ax2.errorbar(T_jl, M_jl, yerr=E_jl, fmt='-', color='purple')
 ax2.plot([250, 350], [-15.5, -14.473955], 'k--')
 Mmin, Mmax = ax2.get_ylim()
 ax1.set_ylim([Mmin + d_J, Mmax + d_J])
 ax2.set_xlim([-10, 450])
-ax1.text(250, 14.5, "J13522", color='r')
-ax1.text(150, 19, "1999em", color='g')
-ax1.text(80, 15.5, "1998S", color='c')
+ax1.text(200, 15.2, "J13522", color='r')
+ax1.text(250, 13.7, "1997cy", color='orange')
+ax1.text(150, 19, "1999em", color='y')
+ax1.text(160, 17.5, "1998S", color='g')
+ax1.text(350, 14.1, "1988Z", color='c')
 ax1.text(250, 18.5, "1987A", color='b')
-ax1.text(300, 17.5, r"$^{56}$Co", color='k')
+ax1.text(170, 13.3, "2010jl", color='purple')
+ax1.text(300, 17.7, r"$^{56}$Co", color='k')
 ax1.invert_yaxis()
 ax2.invert_yaxis()
 ax1.set_xlabel('Days after explosion')
--- json_to_csv.py
+++ json_to_csv.py
@@ -12,6 +12,7 @@ csv = sys.argv[2]
 band = sys.argv[3]
 
 time = []
+e_mag = []
 mag = []
 
 # Read through json to photometry section
@@ -20,7 +21,7 @@ with open(json) as f:
     if "photometry" in line:
       break
   # Extract photometry in specified band
-  t, b, m = "", 0, ""
+  t, b, m, e = "", 0, "", ""
   for line in f:
     if "spectra" in line:
       break
@@ -34,13 +35,19 @@ with open(json) as f:
       else:
         t = ""
         continue
+    if "\"e_mag" in line and t != "" and b == 1:
+      e = line.split("\"")[-2]
+      e_mag.append(e)
     if "\"mag" in line and t != "" and b == 1:
       m = line.split("\"")[-2]
       time.append(t)
       mag.append(m)
-      t, b, m = "", 0, ""
+      t, b, m, e = "", 0, "", ""
 
 # Write photometry to text file
 with open(csv, 'w') as f:
   for i in range(0, len(time)):
-    f.write("%s,%s\n" % (time[i], mag[i]))
+    if len(e_mag) != 0:
+      f.write("%s,%s,%s\n" % (time[i], mag[i], e_mag[i]))
+    else:
+      f.write("%s,%s\n" % (time[i], mag[i]))
